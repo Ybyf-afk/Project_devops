595ea692e3aff95468175e481fd311835e7602f8
Ybyf-afk <57497541+Ybyf-afk@users.noreply.github.com>
2019-12-12 23:13:33 +0300

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -1,4 +1,4 @@
-# Security groups required for devops VPC
+# Security groups required for VPC
 resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
   description = "Allows inbound SSH traffic."
@@ -49,44 +49,44 @@ resource "aws_security_group" "allow_tomcat" {
   }
 }
 
-resource "aws_security_group" "devops-alb" {
-  name = "devops-alb"
-  description = "Security group for ALB."
+# resource "aws_security_group" "devops-alb" {
+#   name = "devops-alb"
+#   description = "Security group for ALB."
 
-  vpc_id = aws_vpc.devops.id
+#   vpc_id = aws_vpc.devops.id
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port = 80
+#     to_port = 80
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port = 0
+#     to_port = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_security_group" "ecs-instance-devops" {
-  name = "ecs-instance-devops"
-  description = "Security group for ALB."
+# resource "aws_security_group" "ecs-instance-devops" {
+#   name = "ecs-instance-devops"
+#   description = "Security group for ALB."
 
-  vpc_id = aws_vpc.devops.id
+#   vpc_id = aws_vpc.devops.id
 
-  ingress {
-    from_port = 32768
-    to_port = 61000
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port = 32768
+#     to_port = 61000
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port = 0
+#     to_port = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
