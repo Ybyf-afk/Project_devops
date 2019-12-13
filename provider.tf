113125aa7b95903fe85fbb75b0b43d8b1a6ca0cc
Ybyf-afk <57497541+Ybyf-afk@users.noreply.github.com>
2019-12-13 10:36:31 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "eu-west-2"
-  access_key = "AKIAI3QZOSHR3I5HL7TQ"
-  secret_key = "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"
+  access_key = "*******"
+  secret_key = "***********************"
 }
